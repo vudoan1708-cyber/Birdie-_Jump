9ba949714849641a84861ef2d5fcc8d8cf0e1a53
Vu Doan <vu@voly.co.uk>
2025-08-27 18:59:20 +0100

    remove trailing forward slashes for API calls

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const app = express();
 
 const path = require('path');
+const express = require('express');
+const Datastore = require('nedb');
+const http = require('http');
 const fallback = require('express-history-api-fallback');
 
-const root = path.join(__dirname, './public');
-const port = process.env.PORT || 5000;
-
-const http = require('http');
-const server = http.createServer(app);
-
-const Datastore = require('nedb');
 const database = new Datastore('database/score.db');
 const database2 = new Datastore('database/score2.db');
 const acc = new Datastore('database/acc.db');
+
 database.loadDatabase();
 database2.loadDatabase();
 acc.loadDatabase();
 
+const root = path.join(__dirname, './public');
+const port = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+
 server.listen(port, () => { console.log(`listening on port: ${port}`) });
 app.use(express.json({ limit: '1mb' }));
 
-app.post('/score/', function (request, response) {
+app.post('/score', function (request, response) {
 
     const data = request.body;
 
@@ -29,7 +30,7 @@ app.post('/score/', function (request, response) {
     response.json(data);
 });
 
-app.get('/score/', function (request, response) {
+app.get('/score', function (request, response) {
     database.find({}).sort({ score: -1 }).exec((err, data) => {
         if (err) {
             console.error(err);
@@ -39,7 +40,7 @@ app.get('/score/', function (request, response) {
     });
 });
 
-app.post('/score2/', function (request, response) {
+app.post('/score2', function (request, response) {
 
     const data = request.body;
 
@@ -47,7 +48,7 @@ app.post('/score2/', function (request, response) {
     response.json(data);
 });
 
-app.get('/score2/', function (request, response) {
+app.get('/score2', function (request, response) {
     database2.find({}).sort({ time: -1 }).exec((err, data) => {
         if (err) {
             console.error(err);
@@ -57,14 +58,14 @@ app.get('/score2/', function (request, response) {
     });
 });
 
-app.post('/acc/', function(request, response) {
+app.post('/acc', function(request, response) {
     const data = request.body;
 
     acc.insert(data);
     response.json(data);
 });
 
-app.get('/acc/', function(request, response) {
+app.get('/acc', function(request, response) {
     acc.find({}).exec((err, data) => {
         if (err) {
             console.error(err);
